dcfe9efecb952beea77834b04cd5ee2221ed555e
805963533 <805963533@qq.com>
2022-04-16 15:03:11 +0800

    commit javadev

---
+++ testproject/src/testJava.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class testJava {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Map myMap = new HashMap<Integer,String>();
+        myMap.put(101,"Adams tang");
+
+    }
+}
